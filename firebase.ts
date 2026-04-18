610310bb44dd8fb7b977accb9ee7a38c15a4ecb4
Ahmet <ahmetheat72@gmail.com>
2026-04-18 11:03:06 +0300

    Protected Firebase API keys

--- firebase.ts
+++ firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCLfFKSVDlFSOwqXZKK4GNQ64wXoPcFATo",
-  authDomain: "contactforms-2f87a.firebaseapp.com",
-  projectId: "contactforms-2f87a",
-  storageBucket: "contactforms-2f87a.firebasestorage.app",
-  messagingSenderId: "257411095702",
-  appId: "1:257411095702:web:9d6aa447d457e62d68b6b7",
-  measurementId: "G-H9M272ER6F"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
